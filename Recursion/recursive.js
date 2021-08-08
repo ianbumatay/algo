9ca71a6c2e8317e9369e5e62cb04f8b9b783db01
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-08-08 16:05:43 -0400

    recursive function working with string

--- Recursion/recursive.js
+++ Recursion/recursive.js
@@ -1,13 +1,42 @@
-// recurcive algo 
+// Recursive Functions => function that call itself
 
 function countDownFrom(n){ 
     console.log(n) 
     if(n > 1){
-        countDownFrom(n -1)
-    } else{
-        return true
+        countDownFrom(n -1) // recursive call 
+    } else{ 
+        console.log("boom done")
+        return true // Base case 
     }
 
 } 
 
-countDownFrom(5) 
+
+function sumUpTo(n){
+    console.log(n) 
+    if(n > 1){
+        sumUpTo(n - 1) + n 
+    } else{
+        return 1
+    }
+
+} 
+
+//sumUpTo(5) 
+
+
+// working with string 
+
+function printString(myString){
+
+    console.log(myString[0]) 
+
+    if(myString.length > 1){
+        let mySubstring = myString.substring(1, myString.length) 
+        printString(mySubstring)
+    }else{ 
+        return true 
+    }
+} 
+
+printString('pizza') 
